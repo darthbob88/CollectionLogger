29ab20a03a8dea26a440ec757830610ee1280522
Raymond Price <darthbob88@gmail.com>
2015-09-20 17:18:48 -0700

    Added a boatload of documentation comments. Also changed a couple parameters in certain methods to be slightly more sensible. Also fixed some config options.

--- LogCollections/Program.cs
+++ LogCollections/Program.cs
@@ -8,22 +8,45 @@ using System.Xml.Linq;
 
 namespace LogCollections
 {
+    /// <summary>
+    /// Catalogs and dumps media collections to an SQLite database and XML files in three different cloud storage folders.
+    /// Really wish I could do it in just one parameterized ParseAndOrganize method that works on movies/comics/etc.
+    /// Everything's so similar in structure, but they're also too different in organization to have the same method handle both music and movies.
+    /// TODO Add more media types and sources, possibly including ebooks and RSS feeds and such.
+    /// TODO Add more storage locations or metadata.
+    /// TODO Add better serialization as well. Will probably require adding more tables for metadata. 
+    /// </summary>
     internal class Program
     {
-        static private readonly string profile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        static private readonly string[] TARGETS = { profile + @"\OneDrive\Documents",
-            profile + @"\Dropbox\Public",
-            profile + @"\Google Drive" };
+        static private readonly string Profile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        /// <summary>
+        /// List of cloud storage folders to which our catalogs will be written. 
+        /// TODO Move to external user-adjustable config, along with the library locations and logfiles.
+        /// TODO Alternatively, find programmatic way to detect which cloud-storage folders are available.
+        /// </summary>
+        static private readonly string[] Targets = { Profile + @"\OneDrive\Documents",
+            Profile + @"\Dropbox\Public",
+            Profile + @"\Google Drive" };
 
         private static void Main()
         {
             ParseAndDumpMovies(@"/movies.xml", @"F:\Movies");
             ParseAndDumpComics(@"/comics.xml", @"F:\Comics");
             ParseAndDumpTV(@"/TV.xml", @"F:\TV Shows");
-            ParseAndDumpPorn(@"/business_material.xml", profile + @"\Videos", @"F:\Videos");
-            ParseAndDumpMusicCollection(@"/music.xml", profile + @"\Music", @"F:\Music");
+            ParseAndDumpPorn(@"/business_material.xml", Profile + @"\Videos", @"F:\Videos");
+            ParseAndDumpMusicCollection(@"/music.xml", Profile + @"\Music", @"F:\Music");
             //Console.ReadLine();
         }
+        /// <summary>
+        /// Reads in a list of movie files for cataloging from <paramref name="libraries"/>.
+        /// Writes list of files to XML file named <paramref name="logFile"/>, in directories specified in
+        /// Targets field of class, and also writes to SQLite database for ease of use with other programs,
+        /// including web frontend for this system.
+        /// TODO Without excess cleverness, restructure as much of this as possible into a single function for all media sets.
+        /// TODO Maybe an AddOrIgnore extension method for DbSet?
+        /// </summary>
+        /// <param name="logFile">File name to which a list of movies will be written for backup purposes.</param>
+        /// <param name="libraries">Directory/ies containing movie collection.</param>
         private async static void ParseAndDumpMovies(string logFile, params string[] libraries)
         {
             if (!libraries.Where(Directory.Exists).Any())
@@ -47,7 +70,17 @@ namespace LogCollections
                 context.SaveChanges();
             }
         }
-
+        /// <summary>
+        /// Reads in a list of comics for cataloging from <paramref name="libraries"/>.
+        /// Writes list of files to XML file named <paramref name="logFile"/>, in directories specified in
+        /// Targets field of class, and also writes to SQLite database for ease of use with other programs,
+        /// including web frontend for this system.
+        /// TODO Without excess cleverness, restructure as much of this as possible into a single function for all media sets.
+        /// TODO Maybe an AddOrIgnore extension method for DbSet?
+        /// TODO Add table for individual issues.
+        /// </summary>
+        /// <param name="logFile">File name to which a list of comics will be written for backup purposes.</param>
+        /// <param name="libraries">Directory/ies containing media collection.</param>
         private static async void ParseAndDumpComics(string logFile, params string[] libraries)
         {
             if (!libraries.Where(Directory.Exists).Any())
@@ -73,13 +106,23 @@ namespace LogCollections
                 context.SaveChanges();
             }
         }
-
-        private async static void ParseAndDumpTV(string logFile, params string[] tvShows)
+        /// <summary>
+        /// Reads in a list of TV episodes for cataloging from <paramref name="libraries"/>.
+        /// Writes list of files to XML file named <paramref name="logFile"/>, in directories specified in
+        /// Targets field of class, and also writes to SQLite database for ease of use with other programs,
+        /// including web frontend for this system.
+        /// TODO Without excess cleverness, restructure as much of this as possible into a single function for all media sets.
+        /// TODO Maybe an AddOrIgnore extension method for DbSet?
+        /// TODO Find some way to parse out the episode number for ID purposes.
+        /// </summary>
+        /// <param name="logFile">File name to which a list of TV episodes will be written for backup purposes.</param>
+        /// <param name="libraries">Directory/ies containing TV shows.</param>
+        private async static void ParseAndDumpTV(string logFile, params string[] libraries)
         {
-            if (!tvShows.Where(Directory.Exists).Any())
+            if (!libraries.Where(Directory.Exists).Any())
                 return;
 
-            var seasons = PullCollection(tvShows).GroupBy(item => item.Split('\\')[0]);
+            var seasons = PullCollection(libraries).GroupBy(item => item.Split('\\')[0]);
             using (var context = new mediaEntities())
             {
                 var TVTree = new XElement("TVCollection");
@@ -106,13 +149,22 @@ namespace LogCollections
                 context.SaveChanges();
             }
         }
-
-        private static async void ParseAndDumpPorn(string logFile, params string[] pornStash)
+        /// <summary>
+        /// Reads in a list of video files for cataloging from <paramref name="libraries"/>.
+        /// Writes list of files to XML file named <paramref name="logFile"/>, in directories specified in
+        /// Targets field of class, and also writes to SQLite database for ease of use with other programs,
+        /// including web frontend for this system.
+        /// TODO Without excess cleverness, restructure as much of this as possible into a single function for all media sets.
+        /// TODO Maybe an AddOrIgnore extension method for DbSet?
+        /// </summary>
+        /// <param name="logFile">File name to which a list of movies will be written for backup purposes.</param>
+        /// <param name="libraries">Directory/ies containing movie collection.</param>
+        private static async void ParseAndDumpPorn(string logFile, params string[] libraries)
         {
-            if (!pornStash.Where(Directory.Exists).Any())
+            if (!libraries.Where(Directory.Exists).Any())
                 return;
 
-            var movies = PullCollection(pornStash);
+            var movies = PullCollection(libraries);
             using (var context = new mediaEntities())
             {
                 var MediaTree = new XElement("filecabinet");
@@ -121,19 +173,29 @@ namespace LogCollections
                     if (!context.porn.Any(item => item.video_name == videoName))
                         context.porn.Add(new porn { video_name = videoName });
 
-                    //TODO Find some way of adding categories.
+                    //TODO Find some way of re-adding categories.
                     MediaTree.Add(new XElement("movie", videoName));
                 }
                 await WriteFilesAsync(MediaTree, logFile);
                 context.SaveChanges();
             }
         }
-
-        async private static void ParseAndDumpMusicCollection(string logFile, params string[] directories)
+        /// <summary>
+        /// Reads in a list of music files for cataloging from <paramref name="libraries"/>.
+        /// Writes list of files to XML file named <paramref name="logFile"/>, in directories specified in
+        /// Targets field of class, and also writes to SQLite database for ease of use with other programs,
+        /// including web frontend for this system.
+        /// TODO Without excess cleverness, restructure as much of this as possible into a single function for all media sets.
+        /// TODO Maybe an AddOrIgnore extension method for DbSet?
+        /// TODO Add song table to database.
+        /// </summary>
+        /// <param name="logFile">File name to which a list of music tracks will be written for backup purposes.</param>
+        /// <param name="libraries">Directory/ies containing music collection.</param>
+        async private static void ParseAndDumpMusicCollection(string logFile, params string[] libraries)
         {
-            if (!directories.Where(Directory.Exists).Any())
+            if (!libraries.Where(Directory.Exists).Any())
                 return;
-            var musicList = PullCollection(directories).Where(item => item.EndsWith(".mp3")).Select(item => item.Split('\\')).GroupBy(item => item[0]);
+            var musicList = PullCollection(libraries).Where(item => item.EndsWith(".mp3")).Select(item => item.Split('\\')).GroupBy(item => item[0]);
             using (var context = new mediaEntities())
             {
                 XElement MusicTree = new XElement("MusicCollection");
@@ -145,7 +207,7 @@ namespace LogCollections
 
                     XElement artistNode = new XElement("artist", new XAttribute("name", artist.Key));
 
-                    var albumList = artist.GroupBy(item => item[1]);
+                    var albumList = artist.GroupBy(song => song[1]);
                     foreach (var album in albumList)
                     {
                         if (currentArtist.album.All(item => item.album_title != album.Key))
@@ -161,7 +223,15 @@ namespace LogCollections
                 await WriteFilesAsync(MusicTree, logFile);
             }
         }
-
+        /// <summary>
+        /// Pulls a list of filenames for media objects in the given collection(s).
+        /// Includes filters to exclude unnecessary metadata files, and trims filenames to
+        /// just the media file and hierarchy within the library. artist/album/MP3,
+        /// without C:long/path/to/media/library/
+        /// TODO Idunno. Find better way of organizing the filters and selects?
+        /// </summary>
+        /// <param name="collections">A list of directories that contain parts of a media collection.</param>
+        /// <returns>An IEnumerable of filenames, ready to be organized into a better hierarchy by the calling method.</returns>
         private static IEnumerable<string> PullCollection(params string[] collections)
         {
             return collections.Where(Directory.Exists).Select(
@@ -175,12 +245,18 @@ namespace LogCollections
                                 .Distinct();
 
         }
+        /// <summary>
+        /// Utility methods for asynchronously dumping our catalogs to XML files. 
+        /// TODO Generalize to accept other objects.
+        /// </summary>
+        /// <param name="data">Data to write. </param>
+        /// <param name="file">File name to create.</param>
+        /// <returns></returns>
         public async static Task WriteFilesAsync(XElement data, string file)
         {
-            var tasks = TARGETS.Where(Directory.Exists).Select(f => WriteAsync(data, f + file));
+            var tasks = Targets.Where(Directory.Exists).Select(f => WriteAsync(data, f + file));
             await Task.WhenAll(tasks);
         }
-
         private static async Task WriteAsync(XElement data, string path)
         {
             using (var fs = new StreamWriter(path))
